4bbb1f987f5b4fc98da36a384920602da92f6698
Jess Pomfret <jpomfret7@gmail.com>
2018-03-18 13:43:44 -0700

    updated capitalization

--- ObjectSizeAndCompression.sql
+++ ObjectSizeAndCompression.sql
@@ -3,18 +3,18 @@ SELECT
 	obj.name as TableName, 
 	ind.name as IndexName,
 	ind.type_desc as IndexType, 
-	pas.row_count as Rows, 
+	pas.row_count as NumberOfRows, 
 	(pas.used_page_count * 8)/1024 as SizeUsedMB, 
 	par.data_compression_desc as DataCompression, 
 	(pas.reserved_page_count * 8)/1024 as SizeReservedMB
 FROM sys.objects obj
 INNER JOIN sys.indexes ind
-	on obj.object_id = ind.object_id
+	ON obj.object_id = ind.object_id
 INNER JOIN sys.partitions par
-	on par.index_id = ind.index_id
-	and par.object_id = obj.object_id
+	ON par.index_id = ind.index_id
+	AND par.object_id = obj.object_id
 INNER JOIN sys.dm_db_partition_stats pas
-	on pas.partition_id = par.partition_id
+	ON pas.partition_id = par.partition_id
 WHERE obj.schema_id <> 4 
 	--AND schema_name(obj.schema_id) = 'schemaName'
 	--AND obj.name = 'tableName'
