b7d800a8d13ea6f2bfed9ee8dc5799d68ce31110
Jess Pomfret <jpomfret7@gmail.com>
2018-05-15 16:31:46 -0700

    added used_page_count

--- ObjectSizeAndCompression.sql
+++ ObjectSizeAndCompression.sql
@@ -1,11 +1,12 @@
 SELECT
 	schema_name(obj.SCHEMA_ID) as SchemaName,
-	obj.name as TableName, 
+	obj.name as TableName,
 	ind.name as IndexName,
-	ind.type_desc as IndexType, 
-	pas.row_count as NumberOfRows, 
-	(pas.used_page_count * 8)/1024 as SizeUsedMB, 
-	par.data_compression_desc as DataCompression, 
+	ind.type_desc as IndexType,
+	pas.row_count as NumberOfRows,
+	pas.used_page_count as UsedPageCount,
+	(pas.used_page_count * 8)/1024 as SizeUsedMB,
+	par.data_compression_desc as DataCompression,
 	(pas.reserved_page_count * 8)/1024 as SizeReservedMB
 FROM sys.objects obj
 INNER JOIN sys.indexes ind
@@ -15,7 +16,7 @@ INNER JOIN sys.partitions par
 	AND par.object_id = obj.object_id
 INNER JOIN sys.dm_db_partition_stats pas
 	ON pas.partition_id = par.partition_id
-WHERE obj.schema_id <> 4 
+WHERE obj.schema_id <> 4
 	--AND schema_name(obj.schema_id) = 'schemaName'
 	--AND obj.name = 'tableName'
 ORDER BY SizeUsedMB desc
